d76d68653b7e618cd2592398d8ca2e334d113885
Arkhana <arnaud.cholet90@sfr.fr>
2022-07-22 23:52:24 +0200

    update

--- includes/langues.sh
+++ includes/langues.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# langues
-OPTS=$("$CMDGETOPT" -o vhns: --long fr,en,de,ru,es,pt,ptbr -n 'parse-options' -- "$@")
+		# langues
+		OPTS=$("$CMDGETOPT" -o vhns: --long fr,en,de,ru,es,pt,ptbr -n 'parse-options' -- "$@")
+
 eval set -- "$OPTS"
 while true; do
-	case "$1" in
-			--fr) GENLANG="fr"; break ;;
-			--en) GENLANG="en"; break ;;
-			--de) GENLANG="de"; break ;;
-			--ru) GENLANG="ru"; break ;;
-			--es) GENLANG="es"; break ;;
-			--pt) GENLANG="pt"; break ;;
-			--ptbr) GENLANG="ptbr"; break ;;
+case "$1" in
+		--fr) GENLANG="fr"; break ;;
+		--en) GENLANG="en"; break ;;
+		--de) GENLANG="de"; break ;;
+		--ru) GENLANG="ru"; break ;;
+		--es) GENLANG="es"; break ;;
+		--pt) GENLANG="pt"; break ;;
+		--ptbr) GENLANG="ptbr"; break ;;
 		*|\?)
-			BASELANG="${LANG:0:2}"
-			
+		BASELANG="${LANG:0:2}"
+	
 		# detection auto
 if [ "$BASELANG" = "fr" ]; then GENLANG="fr"
 	elif [ "$BASELANG" = "en" ]; then GENLANG="en"
